c746c58e41010adc1f0b41296b88ed9de1802f3a
HOME_PC_ZHENG <zhengwenqing2000@outlook.com>
2023-04-09 23:25:48 +0900

    4.10

--- usrviscositycalculate.py
+++ usrviscositycalculate.py
@@ -41,18 +41,18 @@ def unnamed(phase_delay_derivative, coordinate_series):
         # 遍历可以求导的位置点
         copy_initial_visc = initial_visc.copy()
         for cycle_index in range(50):
-            # 定义最大的迭代次数，如果迭代次数超过20，自动退出
+            # 定义最大的迭代次数，如果迭代次数超过50，自动退出
             print('在' + str(i) + '点循环' + str(cycle_index + 1) + '次，此时v_eff=' + str(copy_initial_visc[1]))
             # 采用二分法逼近最终值
             predicted_value = []
-            for visc in copy_initial_visc:
+            for visc in initial_visc:
                 alpha = Alpha_Bessel(cylinder_R, freq_0, visc, coordinate_series)
                 alpha -= alpha[0]
                 alpha = np.abs(alpha)
                 alpha_derivative = Tools.derivative(alpha, coordinate_series, derivative_smoother_factor)[i]
                 predicted_value.append(alpha_derivative)
-            if (predicted_value[0] - phase_delay_derivative) * (
-                    phase_delay_derivative - predicted_value[1]) > 0:  # 如果值在预定的粘度最小值和粘度当前值之前
+            if (predicted_value[0] - phase_delay_derivative[i]) * (
+                    phase_delay_derivative[i] - predicted_value[1]) > 0:  # 如果值在预定的粘度最小值和粘度当前值之前
                 if abs(copy_initial_visc[0] - copy_initial_visc[2]) < prec:
                     # 判断是否收敛
                     print('在第' + str(i) + '点收敛\n============================================')
@@ -60,8 +60,8 @@ def unnamed(phase_delay_derivative, coordinate_series):
                 else:
                     print('标准值:' + str(phase_delay_derivative))
                     print('当前判断值:' + str(predicted_value))
-                    copy_initial_visc[2] = copy_initial_visc[1]
-                    copy_initial_visc[1] = (copy_initial_visc[0] + copy_initial_visc[1]) / 2
+                    initial_visc[2] = copy_initial_visc[1]
+                    initial_visc[1] = (copy_initial_visc[0] + copy_initial_visc[1]) / 2
             elif (predicted_value[1] - phase_delay_derivative) * (
                     phase_delay_derivative - predicted_value[2]) > 0:  # 如果值在预定的粘度当前值和粘度最大值值之前
                 if abs(copy_initial_visc[0] - copy_initial_visc[2]) < prec:
