1fc72ad0e90c46f794984b330bb0bdf96aa97b45
LAB_PC_ZHENG <zhengwenqing2000@outlook.com>
2023-04-26 19:33:59 +0900

    4.26.1

--- 20230426_1000cst_CillionOil/test2.py
+++ 20230426_1000cst_CillionOil/test2.py
@@ -1,39 +1,45 @@
-from pyuvp import uvp
+from pyuvp import uvp, usr
 import matplotlib.pyplot as plt
 import numpy as np
 
-data = uvp.readData("1hz30deg.mfprof")
-analysis = data.createUSRAnalysis()
 
-analysis.cylinderGeom(72.5, 46.69, 11.7)
-analysis.coordsClean(156, 175)
-analysis.timeSlicing(5)
-u_theta = analysis.velTableTheta()
-coordinates_r = analysis.coordSeries
-times = analysis.timeSeries()
-x = [i for i in range(len(coordinates_r))]
-vibration_frequency, max_magnitude, phase_delay, phase_delay_derivative, real_part, imag_part = analysis.doFFT()
-plt.plot(phase_delay_derivative, coordinates_r)
-plt.grid()
-plt.show()
-
-u = np.transpose(u_theta[0:50, :])
-time = times[0:50]
-plt.figure()
-plt.ylabel('R')
-plt.xlabel('t [s]')
-#plt.ylim(0.65, 1)
-plt.contourf(time, coordinates_r, u, cmap="bwr", levels=20)
-plt.colorbar()
-plt.show()
-
-
-visc, shearrate = analysis.calculate_Viscosity_ShearRate(ignoreException=True)
+files = ["05hz60deg.mfprof","05hz75deg.mfprof","1hz45deg.mfprof","1hz30deg.mfprof"]
 
 plt.figure()
 plt.xlabel(r'Shear Rate $\gamma_{\mathrm{eff}}$ ')
 plt.ylabel(r'Viscosity $\nu_{\mathrm{eff}}$')
-plt.ylim(200, 600)
+plt.ylim(500, 1500)
+
+for file in files:
+    data = uvp.readData(file)
+    vel_origin = data.velTables[0]
+
+    analysis = data.createUSRAnalysis()
+    analysis.cylinderGeom(72.5, 46.69, 11.6)
+    analysis.coordsClean(165, 215)
+    analysis.timeSlicing(0)
+    u_theta = analysis.velTableTheta()
+    coordinates_r = analysis.coordSeries
+    times = analysis.timeSeries()
+    '''x = [i for i in range(len(coordinates_r))]
+    vibration_frequency, max_magnitude, phase_delay, phase_delay_derivative, real_part, imag_part = analysis.doFFT()
+    plt.plot(phase_delay_derivative, coordinates_r)
+    plt.grid()
+    plt.show()'''
+
+    '''u = np.transpose(u_theta[0:50, :])
+    time = times[0:50]
+    plt.figure()
+    plt.ylabel('R')
+    plt.xlabel('t [s]')
+    #plt.ylim(0.65, 1)
+    plt.contourf(time, coordinates_r, u, cmap="bwr", levels=20)
+    plt.colorbar()
+    plt.show()'''
+
+    visc, shearrate = analysis.calculate_Viscosity_ShearRate(ignoreException=True)
+    plt.scatter(shearrate, visc)
+
 plt.grid()
-plt.scatter(shearrate, visc)
+
 plt.show()
--- pyuvp/__init__.py
+++ pyuvp/__init__.py
@@ -1,1 +1,2 @@
-
+import pyuvp.usr
+import pyuvp.uvp
