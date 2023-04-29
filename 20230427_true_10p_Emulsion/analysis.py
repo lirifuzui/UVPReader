e33ad74ceea157817d1a8b142d1f98ff741ccc2e
LAB_PC_ZHENG <zhengwenqing2000@outlook.com>
2023-04-29 16:34:19 +0900

    4.29.1

--- 20230427_true_10p_Emulsion/analysis.py
+++ 20230427_true_10p_Emulsion/analysis.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-# files = ["1hz30deg.mfprof","1hz45deg.mfprof","1hz60deg02.mfprof","05hz60deg02.mfprof","05hz90deg.mfprof","05hz120deg.mfprof","05hz150deg02.mfprof"]
+files = ["1hz30deg.mfprof","1hz45deg.mfprof","1hz60deg02.mfprof","05hz60deg02.mfprof","05hz90deg.mfprof","05hz120deg.mfprof","05hz150deg.mfprof"]
 # files = ["1hz30deg.mfprof","1hz45deg.mfprof","1hz60deg02.mfprof"]
 # files = ["05hz60deg02.mfprof","05hz90deg.mfprof","05hz120deg.mfprof","05hz150deg.mfprof"]
-files = ["05hz60deg.mfprof","05hz60deg02.mfprof","05hz60deg03.mfprof"]
+# files = ["05hz60deg.mfprof","05hz60deg02.mfprof","05hz60deg03.mfprof"]
 plt.figure()
 plt.xlabel(r'Shear Rate $\gamma_{\mathrm{eff}}$ ')
 plt.ylabel(r'Viscosity $\nu_{\mathrm{eff}}$')
@@ -20,7 +20,7 @@ for file in files:
     analysis = data.createUSRAnalysis()
     analysis.cylinderGeom(72.5, 71.54, 11.35)
     analysis.coordsClean(55, 90)
-    analysis.timeSlicing(10)
+    analysis.timeSlicing(50)
     u_theta = analysis.velTableTheta()
     coordinates_r = analysis.coordSeries
     times = analysis.timeSeries()
@@ -41,12 +41,12 @@ for file in files:
     plt.show()'''
 
     visc, shearrate = analysis.calculate_Viscosity_ShearRate(smooth_level= 9,ignoreException=True)
-    visc_t = visc.reshape((11,35))
-    shearrate_t = shearrate.reshape((11,35))
+    '''visc_t = visc.reshape((51,35))
+    shearrate_t = shearrate.reshape((51,35))
     visc = np.sum(visc_t,axis=0)/11
-    shearrate = np.sum(shearrate_t,axis=0)/11
+    shearrate = np.sum(shearrate_t,axis=0)/11'''
 
-    plt.scatter(shearrate, visc, label=file)
+    plt.scatter(shearrate, visc, s=10, alpha=0.1, label=file)
 
 
 plt.grid()
