c5e9e434d89f5cffbc84c152db16098984cf5791
LAB_PC_ZHENG <zhengwenqing2000@outlook.com>
2023-04-29 18:10:55 +0900

    4.29.4

--- 20230429_true_20p_Emulsion/analysis.py
+++ 20230429_true_20p_Emulsion/analysis.py
@@ -5,7 +5,7 @@ from pyuvp import uvp
 # files = ["1hz30deg.mfprof","1hz45deg.mfprof","1hz60deg02.mfprof","05hz60deg02.mfprof","05hz90deg.mfprof","05hz120deg.mfprof","05hz150deg02.mfprof"]
 # files = ["1hz30deg.mfprof","1hz45deg.mfprof","1hz60deg02.mfprof"]
 # files = ["05hz60deg02.mfprof","05hz90deg.mfprof","05hz120deg.mfprof","05hz150deg.mfprof"]
-files = ["05hz60deg.mfprof", "05hz90deg.mfprof", "05hz90deg02.mfprof", "05hz120deg.mfprof", "05hz150deg.mfprof",
+files = ["05hz60deg.mfprof", "05hz90deg02.mfprof", "05hz120deg.mfprof", "05hz150deg.mfprof",
          "1hz30deg.mfprof"]
 plt.figure()
 plt.xlabel(r'Shear Rate $\gamma_{\mathrm{eff}}$ ')
