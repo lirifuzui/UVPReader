38e57871c11cf6c605c6f0a4a260cfd881b46755
zheng <zhengwenqing2000@outlook.com>
2023-05-01 10:15:56 +0900

    5.1.1

---
+++ 20230426_1000cst_CillionOil/test2.py
@@ -0,0 +1,45 @@
+from pyuvp import uvp, usr
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+files = ["05hz60deg.mfprof","05hz75deg.mfprof","1hz45deg.mfprof","1hz30deg.mfprof"]
+
+plt.figure()
+plt.xlabel(r'Shear Rate $\gamma_{\mathrm{eff}}$ ')
+plt.ylabel(r'Viscosity $\nu_{\mathrm{eff}}$')
+plt.ylim(0, 2000)
+
+for file in files:
+    data = uvp.readData(file)
+    vel_origin = data.velTables[0]
+
+    analysis = data.createUSRAnalysis()
+    analysis.cylinderGeom(72.5, 46.69, 11.6)
+    analysis.coordsClean(165, 215)
+    analysis.slicing(0)
+    u_theta = analysis.velTableTheta()
+    coordinates_r = analysis.coordSeries
+    times = analysis.timeSeries()
+    '''x = [i for i in range(len(coordinates_r))]
+    vibration_frequency, max_magnitude, phase_delay, phase_delay_derivative, real_part, imag_part = analysis.doFFT()
+    plt.plot(phase_delay_derivative, coordinates_r)
+    plt.grid()
+    plt.show()'''
+
+    '''u = np.transpose(u_theta[0:50, :])
+    time = times[0:50]
+    plt.figure()
+    plt.ylabel('R')
+    plt.xlabel('t [s]')
+    #plt.ylim(0.65, 1)
+    plt.contourf(time, coordinates_r, u, cmap="bwr", levels=20)
+    plt.colorbar()
+    plt.show()'''
+
+    visc, shearrate = analysis.calculation(ignoreException=True)
+    plt.scatter(shearrate, visc)
+
+plt.grid()
+
+plt.show()
---
+++ 20230427_true_10p_Emulsion/analysis.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from pyuvp import uvp
+
+# files = ["1hz30deg.mfprof","1hz45deg.mfprof","1hz60deg02.mfprof","05hz60deg03.mfprof","05hz90deg.mfprof","05hz120deg.mfprof","05hz150deg.mfprof"]
+# files = ["1hz30deg.mfprof","1hz45deg.mfprof","1hz60deg02.mfprof"]
+# files = ["05hz60deg02.mfprof","05hz90deg.mfprof","05hz120deg.mfprof","05hz150deg.mfprof"]
+files = ["05hz60deg.mfprof","05hz60deg02.mfprof","05hz60deg03.mfprof"]
+plt.figure()
+plt.xlabel(r'Shear Rate $\gamma_{\mathrm{eff}}$ ')
+plt.ylabel(r'Viscosity $\nu_{\mathrm{eff}}$')
+plt.ylim(500, 2000)
+
+slice_num = 40
+
+for file in files:
+    data = uvp.readData(file)
+    data.redefineSoundSpeed(1029)
+    vel_origin = data.velTables[0]
+
+    analysis = data.createUSRAnalysis()
+    analysis.cylinderGeom(72.5, 71.54, 11.35)
+    analysis.coordsClean(55, 90)
+    analysis.slicing(slice_num)
+    analysis.sliceSize(5000)
+    u_theta = analysis.velTableTheta()
+    coordinates_r = analysis.coordSeries
+    times = analysis.timeSeries()
+
+    visc, shearrate = analysis.calculation(smooth_level=9, ignoreException=True)
+    visc_t = visc.reshape((slice_num+1,35))
+    shearrate_t = shearrate.reshape((slice_num+1,35))
+    visc = np.sum(visc_t,axis=0)/(slice_num+1)
+    shearrate = np.sum(shearrate_t,axis=0)/(slice_num+1)
+    plt.scatter(shearrate, visc, label=file)
+    # plt.scatter(shearrate, visc, s=10, alpha=0.1, label=file)
+
+
+plt.grid()
+plt.legend()
+plt.show()
