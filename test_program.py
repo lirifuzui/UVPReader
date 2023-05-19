a6af7106c6bf7c882aae458ba55dc6a9927fb4f8
LAB_PC_ZHENG <zhengwenqing2000@outlook.com>
2023-05-19 10:32:16 +0900

    5.18.3

--- test_program.py
+++ test_program.py
@@ -1,13 +1,13 @@
 from pyuvp import uvp
 
 data = uvp.readData("1000min.mfprof")
-# vel = data.velTables[0]
-# anaylsis = data.createUSRAnalysis()
-# anaylsis.cylinderGeom(77, 66, 15)
-# anaylsis.channelRange(77, 110)
-# anaylsis.slicing(0)
+vel = data.velTables[0]
+anaylsis = data.createUSRAnalysis()
+anaylsis.cylinderGeom(77, 66, 15)
+anaylsis.channelRange(77, 110)
+anaylsis.slicing(0)
 # viscoity, shearrate = anaylsis.rheologyViscosity()
-# anaylsis.rheologyViscoelasticity(1000)
+anaylsis.rheologyViscoelasticity(1000, 2000)
 '''
 plt.scatter(shearrate, viscoity, s=3, alpha=0.5)
 plt.grid()
