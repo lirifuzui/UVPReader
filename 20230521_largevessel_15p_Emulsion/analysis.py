a835a7a810801076577a7a7492ee738fa4e9fd9e
LAB_PC_ZHENG <zhengwenqing2000@outlook.com>
2023-05-25 17:21:36 +0900

    5.25.1

--- 20230521_largevessel_15p_Emulsion/analysis.py
+++ 20230521_largevessel_15p_Emulsion/analysis.py
@@ -19,21 +19,21 @@ plt.xlim(1, 15)
 for file in files:
     data = uvp.readData(file)
     # data.redefineSoundSpeed(1029)
-    '''vel_origin = data.velTables[0]
+    vel_origin = data.velTables[0]
     coords_origin = data.coordinateSeries[0]
     analysis = data.createUSRAnalysis()
     analysis.channelRange(65, 100)
     analysis.cylinderGeom(77, 79, 10.615)
-    analysis.slicing(20)
-    analysis.sliceSize(5000)
+    '''analysis.slicing(20)
+    analysis.sliceSize(5000)'''
 
-    #shearrate, visc = analysis.rheologyViscosity(smooth_level=9, ignoreException=True)
-    #plt.scatter(shearrate, visc, s=5, alpha=0.3, label=file)
+    # shearrate, visc = analysis.rheologyViscosity(smooth_level=9, ignoreException=True)
+    # plt.scatter(shearrate, visc, s=5, alpha=0.3, label=file)
 
     analysis.slicing(5)
     analysis.sliceSize(3000)
     shearrate, viscoic, viscoelastic, _ = analysis.rheologyViscoelasticity(1000)
-    plt.scatter(shearrate, viscoelastic, s=5, alpha=0.3, label=file)'''
+    plt.scatter(shearrate, viscoelastic, s=5, alpha=0.3, label=file)
 
 plt.grid()
 # plt.legend()
