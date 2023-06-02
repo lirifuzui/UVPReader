f1f030dca8afc3c7f77b799f9c687560ab93297e
LAB_PC_ZHENG <zhengwenqing2000@outlook.com>
2023-06-02 17:00:42 +0900

    6.2.2

--- test_program.py
+++ test_program.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 from pyuvp import uvp
 
-data = uvp.readUvpFile("uvp073008.mfprof", is_output=True)
+data = uvp.readUvpFile("1000min.mfprof", is_output=False)
 data.defineSoundSpeed(1000)
 vel = data.velTables[0]
 times = data.timeArrays[0]
@@ -12,18 +12,16 @@ plt.figure()
 plt.contour(coords, times, vel)
 plt.show()
 
-'''anaylsis = data.createUSRAnalysis()
+anaylsis = data.createUSRAnalysis()
 anaylsis.cylinderGeom(77, 66, 15)
 anaylsis.channelRange(77, 110)
 anaylsis.slicing(5, 500)
-aa = anaylsis.timeArrays(1)
-bb = anaylsis.velTableTheta(1)
 
 # viscoity, shearrate = anaylsis.rheologyViscosity()
 a, b, c, d = anaylsis.rheologyViscoelasticity(1000, 2000)
 plt.scatter(a, c)
 plt.grid()
-plt.show()'''
+plt.show()
 
 '''
 plt.scatter(shearrate, viscoity, s=3, alpha=0.5)
