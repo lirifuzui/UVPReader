f1f82c71d00b2f43aa95342d90405201dad572c5
zheng <zhengwenqing2000@outlook.com>
2023-06-21 21:44:56 +0900

    5.21.1

--- 未命名0.py
+++ 未命名0.py
@@ -12,8 +12,8 @@ beta = 1+ 4/13*(8*0.637-1)*D_ba*(((5+D_ba)**2*(1-3/8*D_ba/(5+D_ba)))*f_b+
 
 phi_m = (125*f_b + f_s)/((5-D_ba)**3*f_b + (((5+D_ba)**3-(5-D_ba)**3)*f_b+(1+D_ba)**3*f_s)/beta)
 
-
-phi =phi_m/(0.75/(np.sqrt(y_r*((2*y_r + 5*K)/(2+5*K))**(3/2))-1)+1)
+y = y_r*((2*y_r + 5*K)/(2+5*K))**(3/2)
+phi =(1-y**(-1/2.5))*phi_m
 
 plt.figure()
 plt.yscale('log')
