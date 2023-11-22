d5f8c32431ebbd6ffee6db51ecffaedf4731b098
LAB_PC_ZHENG <zhengwenqing2000@outlook.com>
2023-11-22 13:48:14 +0900

    11.08.01

--- 20231003_emulsion_inline/25p/steally.py
+++ 20231003_emulsion_inline/25p/steally.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
+
 from pyuvp import ForMetflowUvp
 
 files = [70, 75, 80, 85, 90]
 diff_P = np.array([-264.9300263, -252.3871998, -250.2671287, -241.0135767, -232.8278945]) + 303.2681338
-
+result = []
 for n, file in enumerate(files):
     # 定义拟合函数
     delta_P = diff_P[n] * 10
@@ -24,11 +25,18 @@ for n, file in enumerate(files):
     coords_origin = data.coordinateArrays[0] * np.cos(30 / 180 * np.pi)
     coords_origin = coords_origin - 27
     coords = coords_origin[30:150]
-    vel = np.mean(vel, axis=0)
-    vel = vel[30:150]
-    params, covariance = curve_fit(velosity_perfile, coords / 1000, vel / 1000, p0=0.5)
-    plt.scatter(coords / 1000, vel / 1000)
-    plt.plot(coords / 1000, velosity_perfile(coords / 1000, params[0]))
-    print(params[0])
+    subarrays = np.vsplit(vel, 20)
+    for subarr in subarrays:
+        subarr = np.mean(subarr, axis=0)
+        subarr = subarr[30:150]
+        params, covariance = curve_fit(velosity_perfile, coords / 1000, subarr / 1000, p0=0.5)
+        plt.scatter(coords / 1000, subarr / 1000)
+        plt.plot(coords / 1000, velosity_perfile(coords / 1000, params[0]))
+        print(params[0])
+        result.append(params[0])
+
+    print('=================')
+result = np.array(result)
+np.savetxt('result.csv', result, delimiter=',')
 plt.grid()
 plt.show()
--- 20231003_emulsion_inline/30p/steally.py
+++ 20231003_emulsion_inline/30p/steally.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
+
 from pyuvp import ForMetflowUvp
 
 files = [70, 75, 80, 85, 90]
 diff_P = np.array([-259.6438709, -248.9572471, -239.7117803, -231.2320523, -220.1030211]) + 302.8222761
-
+result = []
 for n, file in enumerate(files):
     # 定义拟合函数
     delta_P = diff_P[n] * 10
@@ -22,13 +23,20 @@ for n, file in enumerate(files):
     data.defineSoundSpeed(1010)
     vel = data.velTables[0] * 2
     coords_origin = data.coordinateArrays[0] * np.cos(30 / 180 * np.pi)
-    coords_origin = coords_origin - 26
+    coords_origin = coords_origin - 27
     coords = coords_origin[30:120]
-    vel = np.mean(vel, axis=0)
-    vel = vel[30:120]
-    params, covariance = curve_fit(velosity_perfile, coords / 1000, vel / 1000, p0=0.5)
-    plt.scatter(coords / 1000, vel / 1000)
-    plt.plot(coords / 1000, velosity_perfile(coords / 1000, params[0]))
-    print(params[0])
+    subarrays = np.vsplit(vel, 20)
+    for subarr in subarrays:
+        subarr = np.mean(subarr, axis=0)
+        subarr = subarr[30:120]
+        params, covariance = curve_fit(velosity_perfile, coords / 1000, subarr / 1000, p0=0.5)
+        plt.scatter(coords / 1000, subarr / 1000)
+        plt.plot(coords / 1000, velosity_perfile(coords / 1000, params[0]))
+        print(params[0])
+        result.append(params[0])
+
+    print('=================')
+result = np.array(result)
+np.savetxt('result.csv', result, delimiter=',')
 plt.grid()
 plt.show()
