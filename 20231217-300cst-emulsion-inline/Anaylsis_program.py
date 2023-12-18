91cdb151e46e6e92d5c55ea7ae646a4fc10427b1
zheng <zhengwenqing2000@outlook.com>
2023-12-18 16:20:42 +0900

    12.18.02

--- 20231217-300cst-emulsion-inline/Anaylsis_program.py
+++ 20231217-300cst-emulsion-inline/Anaylsis_program.py
@@ -1,56 +1,51 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import panda as pd
+import pandas as pd
 from scipy.optimize import curve_fit
 
 from pyuvp import ForMetflowUvp
 
 Volume_fraction = ["5p", "10p", "15p", "20p", "25p", "30p"]
+# Duty = ["65", "70", "75"]
 Duty = ["60", "65", "70", "75", "80"]
-pressure_0 = -100
+pressure_0 = [-218, -266, -273, -649.6, -652.8, -486.02]
 
-for volume_f in Volume_fraction:
+for n, volume_f in enumerate(Volume_fraction):
+    plt.figure()
     for duty in Duty:
-        csv_file_path = volume_f + duty + 'duty.csv'
-        df = pd.read_csv(csv_file_path)
-        pressure = df.iloc[1:, 6].values
-        pressure = (np.array(pressure) - pressure_0) * 10
-
-        def velosity_perfile(r, miu):
-            return delta_P / (4 * miu * L) * (Rudio ** 2 - (r) ** 2)
-
-        data = ForMetflowUvp.readUvpFile(volume_f + "-" + duty + "duty.mfprof")
-        data.defineSoundSpeed(1010)
-
-for n, file in enumerate(files):
-    # 定义拟合函数
-    delta_P = diff_P[n] * 10
-    Rudio = 0.025
-    L = 0.46
+        try:
+            print(volume_f + duty)
+            csv_file_path = volume_f + duty + 'duty.csv'
+            df = pd.read_csv(csv_file_path)
+            pressure = df.iloc[1:, 6].values
+            pressure = (np.array(pressure) - pressure_0[n]) * 10
+            Rudio = 0.025
+            L = 0.46
+            delta_P = np.mean(pressure)
+            print("delta_P:" + str(delta_P))
 
 
+            def velosity_perfile(r, miu):
+                return delta_P / (4 * miu * L) * (Rudio ** 2 - (r) ** 2)
 
 
+            data = ForMetflowUvp.readUvpFile(volume_f + "-" + duty + "duty.mfprof")
+            data.defineSoundSpeed(1020)
 
-    # 文件数据
-    data = ForMetflowUvp.readUvpFile(str(file) + ".mfprof")
-    data.defineSoundSpeed(1010)
-    vel = data.velTables[0] * 2
-    coords_origin = data.coordinateArrays[0] * np.cos(30 / 180 * np.pi)
-    coords_origin = coords_origin - 29
-    coords = coords_origin[15:60]
-    subarrays = np.vsplit(vel, 20)
-    for subarr in subarrays:
-        subarr = np.mean(subarr, axis=0)
-        subarr = subarr[15:60]
-        params, covariance = curve_fit(velosity_perfile, coords / 1000, subarr / 1000, p0=0.5)
-        plt.scatter(coords / 1000, subarr / 1000)
-        plt.plot(coords / 1000, velosity_perfile(coords / 1000, params[0]))
-        print(params[0])
-        result.append(params[0])
-
-    print('=================')
-result = np.array(result)
-np.savetxt('result.csv', result, delimiter=',')
-plt.grid()
-plt.show()
+            vel = data.velTables[0] * 2
+            coords_origin = data.coordinateArrays[0] * np.cos(30 / 180 * np.pi)
+            coords_origin = coords_origin - 28.7
+            coords = coords_origin[11:165]
+            vel = np.mean(vel, axis=0)
+            vel = vel[11:165]
+            curve_coords = coords_origin[41:135]
+            curve_vel = vel[41:135]
+            params, covariance = curve_fit(velosity_perfile, curve_coords / 1000, curve_vel / 1000, p0=0.5)
+            plt.scatter(coords / 1000, vel / 1000)
+            plt.plot(coords / 1000, velosity_perfile(coords / 1000, params[0]))
+            print("visco:" + str(params[0]))
+        except:
+            continue
+    print('===============================')
+    plt.grid()
+    plt.show()
